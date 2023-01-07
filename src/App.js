7753ba231e0771f80b3b00d0f582267096830168
Amos Weislib <amosweislib@gmail.com>
2023-01-07 12:08:36 +0100

    FreeCodeCamp29

--- src/App.js
+++ src/App.js
@@ -1,29 +1,9 @@
 import React,{ useState } from 'react';
+import freeCodeCamp from './components/freecodecamp';
 
 function App() {
-  const[firstName, setFirstName] = useState("")
-  const[lastName, setLastName] = useState("")
+  <freeCodeCamp />
+}
 
-
-  const firstNameChangeHandler = (event) => {
-    setFirstName(event.target.value)
-  };
-  const lastNameChangeHandler = (event) => {
-    setLastName(event.target.value)
-  };
-  const clickHandler = () =>{
-    alert("Hello " + firstName + " "+ lastName + "!")
-  }
-
-  return (
-    <div>
-      <form>
-        <input type="text" placeholder="First Name" onChange={firstNameChangeHandler}/>
-        <input type="text" placeholder="Last Name" onChange={lastNameChangeHandler}/>
-        <button onClick={clickHandler}>Greet Me!</button>
-        </form>
-    </div>
-  );
-  };
   
 export default App;
