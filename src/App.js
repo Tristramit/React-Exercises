bce7801bb4d61cca5c38a643f2ed9db5b79c82e3
Amos Weislib <amosweislib@gmail.com>
2023-01-15 10:29:37 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import React,{ useState } from 'react';
-import freeCodeCamp from './components/freecodecamp';
+import React from 'react';
+import './App.css';
 
 function App() {
-  <freeCodeCamp />
+  const [showTitle, setShowTitle] = React.useState(true);
+  const onClick = () => setShowTitle(prevState => !prevState);
+
+  return (
+    <div className="App">
+      <button onClick={onClick}>Show/Hide</button>
+      {showTitle ? <h1>Welcome to React Challenges</h1> : null}
+    </div>
+  );
 }
+
 
   
 export default App;
